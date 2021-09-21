8a1782abd4229788698c1d35e258b92aa890944a
Nihal Bidkar <bidkarni@gmail.com>
2021-09-21 15:18:19 +0530

    Add project files.

---
+++ AZWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AZWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
